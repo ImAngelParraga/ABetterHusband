e8fa70598bf0121340dea56ed5359a117e050f42
Angel <im.angelparraga@gmail.com>
2022-06-18 17:01:54 +0200

    Added new colors. Changed light color palette. Created basic main screen composable.

--- app/src/main/java/com/example/abetterhusbandv2/MainActivity.kt
+++ app/src/main/java/com/example/abetterhusbandv2/MainActivity.kt
@@ -3,48 +3,131 @@ package com.example.abetterhusbandv2
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Card
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.core.content.ContextCompat
 import com.example.abetterhusbandv2.ui.theme.ABetterHusbandV2Theme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ABetterHusbandV2Theme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-
-                }
-            }
+            MainScreen()
         }
     }
 }
 
-//Jetpack Compose preview
-@Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
+fun MainScreen() {
     ABetterHusbandV2Theme {
         // A surface container using the 'background' color from the theme
         Surface(
             modifier = Modifier.fillMaxSize(),
             color = MaterialTheme.colors.background
         ) {
+            Scaffold(
+                topBar = {
+                    TopAppBar(
+                        title = {
+                            Text(
+                                text = "A Better Husband",
+                                style = MaterialTheme.typography.h6,
+                                modifier = Modifier.padding(start = 16.dp, end = 16.dp)
+                            )
+                        },
+                        backgroundColor = MaterialTheme.colors.primaryVariant,
+                        elevation = 0.dp
+                    )
+                },
+                floatingActionButton = {
+                    FloatingActionButton(
+                        onClick = {
+                            println("FloatingActionButton clicked")
+                        },
+                        backgroundColor = MaterialTheme.colors.primaryVariant
+                    ) {
+                        Icon(Icons.Filled.Add, "Add new task")
+                    }
+                },
+                content = {
+                    MainContent()
+                }
+            )
+        }
+    }
+}
 
+@Composable
+fun MainContent() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        TaskCard(
+            title = "Task 1",
+            description = "This is a task description",
+        )
+        TaskCard(
+            title = "Task 2",
+            description = "This is a task description",
+        )
+        TaskCard(
+            title = "Task 3",
+            description = "This is a task description",
+        )
+    }
+}
+
+//Jetpack Compose preview
+@Preview
+@Composable
+fun DefaultPreview() {
+    MainScreen()
+}
+
+@Composable
+fun TaskCard(title: String, description: String) {
+    var done by rememberSaveable { mutableStateOf(false) }
+    val bgColor = if (done) ContextCompat.getColor(LocalContext.current, R.color.taskDoneBg)
+                else ContextCompat.getColor(LocalContext.current, R.color.taskUndoneBg)
+    Card(
+        modifier = Modifier
+            .padding(8.dp)
+            .clickable { done = !done },
+        backgroundColor = Color(bgColor),
+        elevation = 16.dp,
+        shape = MaterialTheme.shapes.medium
+    ) {
+        Column(modifier = Modifier.padding(8.dp)) {
+            Text(
+                text = title,
+                style = MaterialTheme.typography.h6,
+                color = Color.White,
+                modifier = Modifier
+                    .align(Alignment.CenterHorizontally)
+                    .padding(bottom = 8.dp)
+            )
+            Text(
+                text = description,
+                style = MaterialTheme.typography.body1,
+                color = Color.White
+            )
         }
     }
 }
--- app/src/main/java/com/example/abetterhusbandv2/ui/theme/Theme.kt
+++ app/src/main/java/com/example/abetterhusbandv2/ui/theme/Theme.kt
@@ -1,6 +1,8 @@
 package com.example.abetterhusbandv2.ui.theme
 
+import android.graphics.Color
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.Colors
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
@@ -13,9 +15,9 @@ private val DarkColorPalette = darkColors(
 )
 
 private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200
+    primary = Orange,
+    primaryVariant = LightBlue,
+    secondary = DarkBlue
 
     /* Other default colors to override
     background = Color.White,

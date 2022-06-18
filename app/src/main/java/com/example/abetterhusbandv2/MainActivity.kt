56b9153dc8144739fecf8765662da934384cc963
Angel <im.angelparraga@gmail.com>
2022-06-18 02:43:45 +0200

    Installed Github Copilot and created preview.

--- app/src/main/java/com/example/abetterhusbandv2/MainActivity.kt
+++ app/src/main/java/com/example/abetterhusbandv2/MainActivity.kt
@@ -3,13 +3,18 @@ package com.example.abetterhusbandv2
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.abetterhusbandv2.ui.theme.ABetterHusbandV2Theme
 
 class MainActivity : ComponentActivity() {
@@ -22,22 +27,24 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
+//Jetpack Compose preview
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     ABetterHusbandV2Theme {
-        Greeting("Android")
+        // A surface container using the 'background' color from the theme
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colors.background
+        ) {
+
+        }
     }
 }
